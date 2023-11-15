8c95a4e400f1e0c0e58114e5b2c6cb3eccf805a6
abdulmomen96 <abdu.galga@gmail.com>
2023-11-15 09:26:51 +0200

    Solve Restore the Array From adjacent Pairs

--- RestoretheArrayFromAdjacentPairs.cpp
+++ RestoretheArrayFromAdjacentPairs.cpp
@@ -6,51 +6,55 @@ public:
         vector<int> result;
         for (auto& innervector:adjacentPairs)
         {
-            vector<int> pair;
 
-            for (auto& element: innervector)
-            {
-                pair.push_back(element);
-                
-            }
-            myHashtable[pair[0]].push_back(pair[1]);
-            myHashtable[pair[1]].push_back(pair[0]);
 
-        }
-        int head;
-        for (const auto& entry : myHashtable) {
-            if (entry.second.size() == 1){
-                head = entry.first;
-                result.push_back(head);
-                head = entry.second[0];
-                break;
-
-            } 
-    }
-    
-    
-    //cout << result[0] << endl;
-    
-    while (true){
-        auto it = myHashtable.find(head);
-        cout << head << endl;
-        if (it -> second.size() == 1)
-        {   
-            result.push_back(it -> second[0]);
-            break;
-        }
-        int temp = it -> second[0];
-        auto it2 = std::find(result.begin(), result.end(), temp);
-        if (it2 != result.end()) result.push_back(temp);
-        else  result.push_back(it -> second[1]);
-        head = result.back();
-        
-        
-
-    }
-
-    
             
+            myHashtable[innervector[0]].push_back(innervector[1]);
+            myHashtable[innervector[1]].push_back(innervector[0]);
+
+        }
+    int head;
+    for (const auto& entry : myHashtable) {
+        if (entry.second.size() == 1){
+            head = entry.first;
+
+            break;
+
+        } 
+
+        
+    }
+    
+    
+    int left = -999999;
+
+    int i = 0;
+    for (int i = 0; i < adjacentPairs.size() + 1; i++){
+        //auto it = myHashtable.find(head);
+        
+        result.push_back(head);
+        
+        
+        
+        int temp = myHashtable[head][0];
+        //auto it2 = std::find(result.begin(), result.end(), temp);
+    
+        if (temp != left) 
+        {
+    
+            head = temp;
+        }
+
+        else 
+            if (myHashtable[head].size() != 1) head = myHashtable[head][1];
+        else 
+            break;
+
+        left = result.back();
+
+    }
+    
+
     return result;
 
     }
